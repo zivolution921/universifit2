4d9b0ce2253fbceb518db27de8f0d04c56958883
Ziv Zamechek <ziv921@yahoo.com>
2016-06-12 16:47:39 -0400

    add bootstrap

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.feature "Creating Home Page" do
   scenario do
      visit '/'
+     
      expect(page).to have_link('Athletes Den')
      expect(page).to have_link('Home')
      expect(page).to have_content('Workout Lounge')
