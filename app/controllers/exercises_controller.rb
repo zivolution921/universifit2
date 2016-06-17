b7b9a937bf57bcba1f07571007aaab9b2dd5fdce
Ziv Zamechek <ziv921@yahoo.com>
2016-06-17 15:46:19 -0400

    Implement destroy action

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -40,6 +40,12 @@ class ExercisesController < ApplicationController
 
   end
 
+  def destroy
+    @exercise.destroy
+    flash[:success] = "Exercise has been deleted"
+    redirect_to user_exercises_path(current_user)
+  end
+
   private
 
   def exercise_params
